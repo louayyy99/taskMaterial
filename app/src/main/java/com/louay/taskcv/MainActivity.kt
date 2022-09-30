dbdfd1181811995a0aab762ed22682f2b062de9d
louayyy99 <louay.labidi@esprit.tn>
2022-09-30 21:04:02 +0100

    splash screen

--- app/src/main/java/com/louay/taskcv/MainActivity.kt
+++ app/src/main/java/com/louay/taskcv/MainActivity.kt
@@ -2,10 +2,13 @@ package com.louay.taskcv
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val splashScreen = installSplashScreen()
+
         setContentView(R.layout.activity_main)
     }
 }
